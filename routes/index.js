5b42425bb07aae0829ae1a3eb4e1fa925e25d326
Siddharth Pandya <sid.pandya66@gmail.com>
2019-02-09 22:10:07 -0500

    added ExpressJS support and routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET ContactMe page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
 module.exports = router;
